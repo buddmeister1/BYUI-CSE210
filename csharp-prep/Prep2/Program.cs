7bcb365ff09c9e27c19eb471d657d3f24e2fa610
Austin Budd <nationalchamp242@gmail.com>
2026-04-23 15:12:35 -0600

    Prep 2 Letter Grade- No Stretch

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage? ");
+        string input = Console.ReadLine();
+
+        int score = int.Parse(input);
+
+        string lett_grade;
+
+        if (score >= 90)
+        {
+            lett_grade ="A";
+            }
+        else if (score >= 80) 
+        {
+                lett_grade = "B";
+            }
+        else if (score >= 70) {
+             lett_grade = "C";
+        }
+        else if (score >= 60)
+        {
+            lett_grade = "D";
+        }
+        else
+        {
+            lett_grade = "F";
+        }
+        Console.WriteLine($"Your grade: {lett_grade}");
+
+        if (score >=70)
+            Console.WriteLine("Well done, you passed this course! ");
+        else
+        {
+            Console.Write("Sorry, you failed this course. Better luck next time.");
+        }   
     }
 }
